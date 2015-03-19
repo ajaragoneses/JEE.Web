21384804ec1f68613dc12997e5dd787385328ce9
ajaragoneses <ajaragoneses@localhost.localdomain>
2015-03-19 10:54:37 +0100

    [Update TemaDao] Corregida una errata con los import

--- src/main/java/es/ajaragoneses/votacion/models/daos/TemaDao.java
+++ src/main/java/es/ajaragoneses/votacion/models/daos/TemaDao.java
@@ -1,6 +1,6 @@
 package es.ajaragoneses.votacion.models.daos;
 
-import es.ajaragoneses.votacion.Models.Entities.Tema;
+import es.ajaragoneses.votacion.models.entities.Tema;
 
 public interface TemaDao extends GenericDao<Tema, Integer> {
 
