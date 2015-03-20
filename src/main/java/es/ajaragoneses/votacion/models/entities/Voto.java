45651e7bd0414381fe2805768e9bee4e7dfeab77
ajaragoneses <ajaragoneses@localhost.localdomain>
2015-03-20 17:14:29 +0100

    [Update Tema] Añadidos los métodos hashCode y equals

--- src/main/java/es/ajaragoneses/votacion/models/entities/Voto.java
+++ src/main/java/es/ajaragoneses/votacion/models/entities/Voto.java
@@ -82,5 +82,48 @@ public class Voto implements Serializable {
 						", NivelEstudios="+this.nivelEstudios.name()+"]";
 	}
 
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((IP == null) ? 0 : IP.hashCode());
+		result = prime * result + ((Valor == null) ? 0 : Valor.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((nivelEstudios == null) ? 0 : nivelEstudios.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Voto other = (Voto) obj;
+		if (IP == null) {
+			if (other.IP != null)
+				return false;
+		} else if (!IP.equals(other.IP))
+			return false;
+		if (Valor == null) {
+			if (other.Valor != null)
+				return false;
+		} else if (!Valor.equals(other.Valor))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nivelEstudios != other.nivelEstudios)
+			return false;
+		return true;
+	}
+
    
 }
