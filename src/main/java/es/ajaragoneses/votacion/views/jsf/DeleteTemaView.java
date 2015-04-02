f6b8d0e3766b01990356f5eca2cd2db21e3f02be
Alvaro <Alvaro@Alvaro-PC>
2015-04-02 13:02:38 +0200

    [Update class DeleteTemaView] Actualizada la clase DeleteTemaView para cumplimentar el caso de uso borrar tema

--- src/main/java/es/ajaragoneses/votacion/views/jsf/DeleteTemaView.java
+++ src/main/java/es/ajaragoneses/votacion/views/jsf/DeleteTemaView.java
@@ -1,23 +1,73 @@
 package es.ajaragoneses.votacion.views.jsf;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import es.ajaragoneses.votacion.models.daos.DaoFactory;
+import es.ajaragoneses.votacion.models.daos.TemaDao;
+import es.ajaragoneses.votacion.models.daos.JPA.DaoJPAFactory;
+import es.ajaragoneses.votacion.models.entities.Tema;
+
+
+@ManagedBean
+@ViewScoped
 public class DeleteTemaView implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	private int code;
 	private boolean autorizado = false;
+	private boolean mostrarMensajeError = false;
+	private int temaId = -1;
+	private List<Tema> listaTemas;
+	private TemaDao temaDao;
+	private boolean mostrarMensajeBorrado;
+	private Logger log = LogManager.getLogger(DeleteTemaView.class);
+
 	
 	
 	public DeleteTemaView(){
-		
+		DaoFactory.setFactory(new DaoJPAFactory());
+		temaDao = DaoFactory.getFactory().getTemaDao();
+		listaTemas = temaDao.findAll();
+		log.info(listaTemas.size());
 	}
 	
 	public String process(){
-		if(code == 666){
+		mostrarMensajeError = false;
+		mostrarMensajeBorrado = false;
+		if(autorizar() && !autorizado){
 			autorizado = true;
+			log.info("Autorizando...");
+			return null;
+		}
+		if(AllFieldsNotNUll()){
+			temaDao.deleteById(temaId);
+			log.info("procesando borrado del id: " + temaId);
+			mostrarMensajeBorrado = true;
+		} else {
+			mostrarMensajeError = true;
 		}
 		return null;
+	}
+
+	private boolean AllFieldsNotNUll() {
+		log.info(temaId);
+		if(code != 666) return false;
+		log.info("code == 666");
+		if(temaId == -1) return false;
+		log.info("temaId: " + temaId);
+		return true;
+	}
+
+	private boolean autorizar() {
+		if(code != 666) return false;
+		return true;
 	}
 
 	public int getCode() {
@@ -34,5 +84,37 @@ public class DeleteTemaView implements Serializable{
 
 	public void setAutorizado(boolean autorizado) {
 		this.autorizado = autorizado;
+	}
+
+	public int getTemaId() {
+		return temaId;
+	}
+
+	public void setTemaId(int temaId) {
+		this.temaId = temaId;
+	}
+
+	public boolean isMostrarMensajeError() {
+		return mostrarMensajeError;
+	}
+
+	public void setMostrarMensajeError(boolean mostrarMensajeError) {
+		this.mostrarMensajeError = mostrarMensajeError;
+	}
+
+	public List<Tema> getListaTemas() {
+		return listaTemas;
+	}
+
+	public void setListaTemas(List<Tema> listaTemas) {
+		this.listaTemas = listaTemas;
+	}
+
+	public boolean isMostrarMensajeBorrado() {
+		return mostrarMensajeBorrado;
+	}
+
+	public void setMostrarMensajeBorrado(boolean mostrarMensajeBorrado) {
+		this.mostrarMensajeBorrado = mostrarMensajeBorrado;
 	}
 }
