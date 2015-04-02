e9e9564256a6f14b19d19039743b22146fcc34be
Alvaro <Alvaro@Alvaro-PC>
2015-04-02 11:55:02 +0200

    [initial] Creada la vista para la clase DeleteTemaView

--- src/main/java/es/ajaragoneses/votacion/views/jsf/DeleteTemaView.java
+++ src/main/java/es/ajaragoneses/votacion/views/jsf/DeleteTemaView.java
@@ -5,5 +5,34 @@ import java.io.Serializable;
 public class DeleteTemaView implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	private int code;
+	private boolean autorizado = false;
+	
+	
+	public DeleteTemaView(){
+		
+	}
+	
+	public String process(){
+		if(code == 666){
+			autorizado = true;
+		}
+		return null;
+	}
 
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public boolean isAutorizado() {
+		return autorizado;
+	}
+
+	public void setAutorizado(boolean autorizado) {
+		this.autorizado = autorizado;
+	}
 }
