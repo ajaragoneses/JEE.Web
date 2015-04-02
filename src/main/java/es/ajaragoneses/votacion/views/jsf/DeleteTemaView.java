dc01d2c4758ad5e4f89ce9c20e3e321ab910d1eb
Alvaro <Alvaro@Alvaro-PC>
2015-04-02 10:44:48 +0200

    [finish] Creada la clase DeleteTemaView

---
+++ src/main/java/es/ajaragoneses/votacion/views/jsf/DeleteTemaView.java
@@ -0,0 +1,9 @@
+package es.ajaragoneses.votacion.views.jsf;
+
+import java.io.Serializable;
+
+public class DeleteTemaView implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+}
